300c08e59c3dc77b7854c0848d3d9dd8168eca6f
Kevin Alcerro <kevin1alcerro@gmail.com>
2019-07-21 15:45:02 -0500

    feat:  app.js added to the nav for mobiles

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@
 var menu = document.querySelector('#menu');
 var main = document.querySelector('main');
 var drawer = document.querySelector('.nav');
+var navitem1 = document.querySelectorAll('.nav__item')[0];
+var navitem2 = document.querySelectorAll('.nav__item')[1];
+var navitem3 = document.querySelectorAll('.nav__item')[2];
 
 menu.addEventListener('click', function (e) {
     drawer.classList.toggle('open');
@@ -12,3 +15,16 @@ menu.addEventListener('click', function (e) {
 main.addEventListener('click', function () {
     drawer.classList.remove('open');
 });
+
+navitem1.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+navitem2.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+navitem3.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+
+
+
